abfc1b46a3a52e1ad0af4defdc46a49d7c31d42d
Aubrey Chipman <aubrey.chipman@gmail.com>
2015-07-25 23:18:34 -0600

    Move to single page application; alert on newly created user

--- src/main/java/loginkata/controller/UserController.java
+++ src/main/java/loginkata/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Valid;
@@ -17,36 +16,30 @@ import javax.validation.Valid;
 @Controller
 @Slf4j
 public class UserController extends WebMvcConfigurerAdapter {
-
     @Autowired
     UserRepository userRepository;
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/results").setViewName("results");
-    }
-
     @RequestMapping(value="/", method=RequestMethod.GET)
-    public String showForm(User user, Model model) {
+    public String index(User user, Model model) {
         model.addAttribute("users", userRepository.findAll());
 
         return "form";
     }
 
     @RequestMapping(value="/", method=RequestMethod.POST)
-    public String checkUserInfo(@Valid User user, BindingResult bindingResult, Model model) {
+    public String createUser(@Valid User user, BindingResult bindingResult, Model model) {
         if(userRepository.exists(user.getUsername())) {
             bindingResult.rejectValue("username", "error.username", "username already exists");
         }
 
-        if (bindingResult.hasErrors()) {
-            return "form";
+        if (!bindingResult.hasErrors()) {
+            log.info("Creating user: " + user.toString());
+            userRepository.save(user);
+
+            model.addAttribute("newUser", user);
         }
 
-        log.info("Creating user: " + user.toString());
-        userRepository.save(user);
-
-        return "redirect:/results";
+        return index(user, model);
     }
 
 }
